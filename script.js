c61f4ab0ff1558aea9aa2e816343bce1a721b921
WAM <eraserw@gmail.com>
2022-11-09 17:05:56 +0100

    essaiJS

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let list = [];
+let list = ['Terry','Sokunthy','Sandrine','Kevin',
+'Johann','Jessie','Jean-Sebastien','Jean-François','Jean-Christophe',
+'Arnaud','Stéphane','Alexandre','Delphine','Laure','Pierre','Sebastien'];
+
+let Bouton = document.createElement("BOUTON");
+let text = document.createTextNode("RANDOM APPRENANT");
+
+Bouton.appendChild(text);
+document.body.appendChild(Bouton);
